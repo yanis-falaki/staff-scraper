55237e533343eeb8d005181374e3a51f6c94fd01
Yanis Falaki <yanisfalaki@outlook.com>
2023-07-13 20:16:59 -0400

    added scraping for vanier

---
+++ vanier.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import csv
+from bs4 import BeautifulSoup
+import string
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for letter in string.ascii_uppercase:
+    driver.get(f'https://www.vaniercollege.qc.ca/staff-directory/?index={letter}')
+
+    page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source, 'html.parser')
+    table = soup.find('table', class_="directoryTable")
+
+    try: rows = table.find_all('tr')
+    except: continue
+
+    if letter == 'A':
+        mode = 'w'  # Overwrite the file for letter 'A'
+    else:
+        mode = 'a'  # Append to the file for other letters
+
+    with open('data/vanier.csv', mode, newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        if mode == 'w':
+            # Write the header row only for letter 'A'
+            header_row = rows[0]
+            headers = [cell.text.strip() for cell in header_row.find_all('th')]
+            writer.writerow(headers)
+
+        # Iterate over the data rows and append '@vaniercollege.qc.ca' to the second cell
+        for row in rows[1:]:
+            cells = row.find_all('td')
+            row_data = [cell.text.strip() + '@vaniercollege.qc.ca' if i == 1 else cell.text.strip() for i, cell in enumerate(cells)]
+            writer.writerow(row_data)
+
+
+
+# Close the browser
+driver.quit()
